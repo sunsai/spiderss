23afdb7ca21a18ac1c608efcfebf7010a1387b76
sai <249752144@qq.com>
2016-07-15 08:43:41 +0800

    update 7152

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // view engine setup
-
+var engin = require('ejs')
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('.html',require('ejs').__express);
+app.set('view engine', 'html');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
